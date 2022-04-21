9d12abf2399b9905df635b83aaa0f23e44255449
HamidMammadov <hamidvm@code.edu.az>
2022-04-21 18:38:18 +0400

    First Commit

---
+++ P224Allup/Areas/Manage/Controllers/ProductController.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P224Allup.DAL;
+using P224Allup.Extensions;
+using P224Allup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P224Allup.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class ProductController : Controller
+    {
+        private readonly AllupDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+
+        public ProductController(AllupDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        public async Task<IActionResult> Index(bool? status, int page = 1)
+        {
+            ViewBag.Status = status;
+
+            IEnumerable<Product> products = await _context.Products
+                .Include(t => t.Brand)
+                .Include(t => t.Category)
+                .Include(t => t.ProductTags).ThenInclude(pt=>pt.Tag)
+                .Where(t => status != null ? t.IsDeleted == status : true )
+                .OrderByDescending(t => t.CreatedAt)
+                .ToListAsync();
+
+            ViewBag.PageIndex = page;
+            ViewBag.PageCount = Math.Ceiling((double)products.Count() / 5);
+
+            return View(products.Skip((page - 1) * 5).Take(5));
+        }
+
+        public async Task<IActionResult> Create(bool? status, int page = 1)
+        {
+            ViewBag.Brands = await _context.Brands.Where(b => !b.IsDeleted).ToListAsync();
+            ViewBag.Categories = await _context.Categories.Where(b => !b.IsDeleted).ToListAsync();
+            ViewBag.Tags = await _context.Tags.Where(t => !t.IsDeleted).ToListAsync();
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product, bool? status, int page = 1)
+        {
+            ViewBag.Brands = await _context.Brands.Where(b => !b.IsDeleted).ToListAsync();
+            ViewBag.Categories = await _context.Categories.Where(b => !b.IsDeleted).ToListAsync();
+            ViewBag.Tags = await _context.Tags.Where(t => !t.IsDeleted).ToListAsync();
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (!await _context.Brands.AnyAsync(b=>b.Id == product.BrandId && !b.IsDeleted))
+            {
+                ModelState.AddModelError("BrandId", "Duzgun Brand Secin");
+                return View();
+            }
+
+            if (!await _context.Categories.AnyAsync(b => b.Id == product.CategoryId && !b.IsDeleted))
+            {
+                ModelState.AddModelError("CategoryId", "Duzgun Category Secin ");
+                return View();
+            }
+
+            if (product.TagIds.Count > 0)
+            {
+                List<ProductTag> productTags = new List<ProductTag>();
+
+                foreach (int item in product.TagIds)
+                {
+                    if (!await _context.Tags.AnyAsync(t=>t.Id != item && !t.IsDeleted))
+                    {
+                        ModelState.AddModelError("TagIds", $"Secilen Id {item} - li Tag Yanlisdir");
+                        return View();
+                    }
+
+                    ProductTag productTag = new ProductTag
+                    {
+                        TagId = item
+                    };
+
+                    productTags.Add(productTag);
+                }
+
+                product.ProductTags = productTags;
+            }
+
+            if (product.MainImageFile != null)
+            {
+                if (!product.MainImageFile.CheckFileContentType("image/jpeg"))
+                {
+                    ModelState.AddModelError("MainImageFile", "Secilen Seklin Novu Uygun");
+                    return View();
+                }
+
+                if (!product.MainImageFile.CheckFileSize(300))
+                {
+                    ModelState.AddModelError("MainImageFile", "Secilen Seklin Olcusu Maksimum 30 Kb Ola Biler");
+                    return View();
+                }
+
+                product.MainImage = product.MainImageFile.CreateFile(_env, "assets", "images", "product");
+            }
+            else
+            {
+                ModelState.AddModelError("MainImageFile", "Main Sekil Mutleq Secilmelidir");
+                return View();
+            }
+
+            if (product.HoverImageFile != null)
+            {
+                if (!product.HoverImageFile.CheckFileContentType("image/jpeg"))
+                {
+                    ModelState.AddModelError("HoverImageFile", "Secilen Seklin Novu Uygun");
+                    return View();
+                }
+
+                if (!product.HoverImageFile.CheckFileSize(300))
+                {
+                    ModelState.AddModelError("HoverImageFile", "Secilen Seklin Olcusu Maksimum 30 Kb Ola Biler");
+                    return View();
+                }
+
+                product.HoverImage = product.HoverImageFile.CreateFile(_env, "assets", "images", "product");
+            }
+            else
+            {
+                ModelState.AddModelError("HoverImageFile", "Hover Sekil Mutleq Secilmelidir");
+                return View();
+            }
+
+            if (product.ProductImagesFile.Count() > 0)
+            {
+                List<ProductImage> productImages = new List<ProductImage>();
+
+                foreach (IFormFile file in product.ProductImagesFile)
+                {
+                    if (!file.CheckFileContentType("image/jpeg"))
+                    {
+                        ModelState.AddModelError("ProductImagesFile", "Secilen Seklin Novu Uygun");
+                        return View();
+                    }
+
+                    if (!file.CheckFileSize(300))
+                    {
+                        ModelState.AddModelError("ProductImagesFile", "Secilen Seklin Olcusu Maksimum 30 Kb Ola Biler");
+                        return View();
+                    }
+
+                    ProductImage productImage = new ProductImage
+                    {
+                        Image = file.CreateFile(_env, "assets", "images", "product"),
+                        CreatedAt = DateTime.UtcNow.AddHours(4)
+                    };
+
+                    productImages.Add(productImage);
+                }
+
+                product.ProductImages = productImages;
+            }
+            else
+            {
+                ModelState.AddModelError("ProductImagesFile", "Product Images File Sekil Mutleq Secilmelidir");
+                return View();
+            }
+
+            product.Availability = product.Count > 0 ? true : false;
+            product.CreatedAt = DateTime.UtcNow.AddHours(4);
+
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("index",new { status,page});
+        }
+    }
+}
